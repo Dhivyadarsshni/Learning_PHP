943bb630b33d2fbd3f22c610fa2107a8dcbb5ef7
Dhivyadarsshni <dhivyadarsshni16@gmail.com>
2025-08-05 17:10:46 +0530

    added arithmetic operations file

---
+++ arithmeticOperations.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Arithmetic Operations</h2>
+    <hr>
+    <?php
+    
+    /*
+    Arithmetic Operators
+
+    + - * / %
+    */
+
+    //addition 
+    echo "Addition: <br>";
+    $x1 = 10;
+    $y1 = 7;
+
+    $z1 = null;
+
+    $z1 = $x1 + $y1;
+    echo "$x1 + $y1 = $z1";
+    echo "<br>";
+
+    //subtraction
+    echo "<br>";
+    echo "Subtraction: <br>";
+    $x2 = 10;
+    $y2 = 7;
+
+    $z2 = null;
+
+    $z2 = $x2 - $y2;
+    echo "$x2 - $y2 = $z2";
+    echo "<br>";
+
+    //multiplication
+    echo "<br>";
+    echo "Multiplication: <br>";
+    $x3 = 10;
+    $y3 = 7;
+
+    $z3 = null;
+
+    $z3 = $x3 * $y3;
+    echo "$x3 * $y3 = $z3";
+    echo "<br>";
+
+    //division
+    echo "<br>";
+    echo "Division: <br>";
+    $x4 = 10;
+    $y4 = 7;
+
+    $z4 = null;
+
+    $z4 = $x4 / $y4;
+    echo "$x4 / $y4 = $z4";
+    echo "<br>";
+
+    //Modulo
+    echo "<br>";
+    echo "Modulo: <br>";
+    $x5 = 10;
+    $y5 = 5;
+
+    $z5 = null;
+
+    $z5 = $x5 / $y5;
+    echo "$x5 / $y5 = $z5";
+    echo "<br>";
+    
+    ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-<?php
-    $name = "Dhivyadarsshni";
-    echo "Hello {$name},<br> This is my first program in php <br>";
-    echo "Learning php is easy <br><br>";
-
-    $age = 21;
-    $email ="dhivya@hotmail.com";
-    echo "Hii {$name} <br> You are {$age} years old <br> Your mail id is {$email}";
-
-    //single line comment
-
-    /*
-    this
-    is 
-    multi-line
-    comment
-    */
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,7 +6,33 @@
     <title>Document</title>
 </head>
 <body>
-    <br>
-    <button>Click me</button>
+    <!-- writing php inside html body -->
+    <?php
+    //writing html code inside php
+        echo "<h3> Hello World</h3>";
+        echo "<hr>";
+        $name = "Dhivyadarsshni";
+        //{ } is a placeholder , optional but it is a good practice
+        echo "Hello $name ,<br> This is my first program in php <br>";
+        echo "Learning php is easy <br><br>";
+
+    //learning comments
+
+    //  1)single line comment in php
+
+    /*
+        2)this
+        is 
+        multi-line
+        comment
+        in php
+    */
+
+    $age = 21;
+    $email ="dhivya@hotmail.com";
+    echo "Hii {$name} <br> You are {$age} years old <br> Your mail id is {$email}";
+
+    
+?>
 </body>
 </html>
