57eb83ebbbd6a49af598c60791cfd45fbf00e62e
Dhivyadarsshni <dhivyadarsshni16@gmail.com>
2025-08-05 16:33:28 +0530

    editing the index.php file

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
     $name = "Dhivyadarsshni";
     echo "Hello {$name},<br> This is my first program in php <br>";
-    echo "Learning php is easy";
+    echo "Learning php is easy <br><br>";
+
+    $age = 21;
+    $email ="dhivya@hotmail.com";
+    echo "Hii {$name} <br> You are {$age} years old <br> Your mail id is {$email}";
+
+    //single line comment
+
+    /*
+    this
+    is 
+    multi-line
+    comment
+    */
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Click me</button>
+</body>
+</html>
