52c9402ec57df5398b9b5f120936d760e7811d86
Dhivyadarsshni_dd <85487484+Dhivyadarsshni@users.noreply.github.com>
2025-09-18 09:33:49 +0530

    include keyword in PHP

---
+++ Body.php
@@ -0,0 +1,17 @@
+<?php
+
+include "Title.php";
+
+echo "<h2>This is the main body</h2>";
+
+echo<<<HTML
+<p style="color:red;" >
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum lacus lectus, quis porta elit sagittis ac. Curabitur dignissim dolor leo, sed fermentum quam lobortis eget. Nulla feugiat sollicitudin augue eget volutpat. Suspendisse leo dui, porta id tortor ac, blandit varius arcu. Curabitur tempor bibendum ex, vitae laoreet ipsum pellentesque congue. Donec non ante egestas, convallis diam ac, egestas dui. Fusce dignissim lacus vitae nisi venenatis, placerat tempor metus semper. Mauris ut laoreet mi, vel fringilla nulla. In ligula eros, facilisis vitae tempor ut, congue at lorem. Sed tincidunt rhoncus orci sed malesuada. Duis ligula odio, dictum sed nulla eu, luctus feugiat felis. Nunc lectus ipsum, pulvinar at felis nec, pellentesque tempus dui.
+
+Quisque at pretium lacus. Morbi efficitur quam vel consectetur congue. Nulla facilisi. Suspendisse et ligula fermentum, viverra urna eget, fermentum tellus. Cras sodales commodo mauris. Suspendisse dapibus varius dui ut iaculis. Nam eget lorem non sapien euismod tempus.
+
+Sed pretium volutpat lorem, ac vehicula odio bibendum eu. Maecenas et interdum enim. Sed consequat ut mauris eget blandit. Aliquam venenatis tempus mi, et mattis ante auctor sed. Fusce est ligula, accumsan vitae ipsum ut, convallis tincidunt magna. Maecenas pharetra eu est eget ullamcorper. Vivamus semper eu magna quis scelerisque. Pellentesque placerat, eros vitae egestas mattis, libero ex convallis nisi, ac iaculis sem dolor vel arcu. Donec velit nibh, tempus ut nulla a, porta eleifend tortor. In scelerisque magna ligula, id aliquam enim varius et.
+
+Nam sollicitudin ullamcorper eros ultricies semper. Phasellus eget felis dignissim, tempus ante et, mollis augue. Duis eleifend sodales euismod. Suspendisse et egestas quam. Nam dictum leo vel lectus pellentesque rhoncus. Curabitur enim erat, pellentesque consequat interdum ac, sagittis ut lacus. Nulla dapibus ornare justo suscipit feugiat. Duis vitae tempus dui. Duis volutpat tellus id ipsum ornare, ac lacinia sem gravida. Proin id venenatis libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam rhoncus eros sed pretium fermentum. Mauris ut dolor purus.
+</p>
+HTML;
---
+++ Title.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include in php</title>
+</head>
+<body>
+    <h2 style="color:blue; text-align:center; "> include keyword in php</h2>
+
+</body>
+</html>
