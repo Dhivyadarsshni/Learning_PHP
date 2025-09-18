97c8c72071c5939450ed22177b81ce9c0500dc50
Dhivyadarsshni <dhivyadarsshni16@gmail.com>
2025-09-18 08:33:03 +0530

    constants in php

---
+++ Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+define("PI",3.14);
+
+echo "<h2> This is PI value: " . PI ;
+
+#tryna change value, but leads to error
+
+//define("PI",2.14);
+
+#this works as an variable
+$PI =2.14;
+
+echo "<br>". $PI;
